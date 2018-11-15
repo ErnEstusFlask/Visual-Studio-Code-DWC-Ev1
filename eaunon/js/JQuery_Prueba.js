340a57aee92c8c1b8f7e3b6bd48a4e9fa8c93991
Ernesto <ernesto.aunon09@gmail.com>
2018-11-15 19:15:03 +0100

    Ej Jquery

--- eaunon/js/JQuery_Prueba.js
+++ eaunon/js/JQuery_Prueba.js
@@ -1,75 +1,111 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    var close=document.getElementById("close");
+
+    //Banner
+    var close = document.getElementById("close");
     close.addEventListener("click", cerrar_banner, false);
-    
-    function cerrar_banner(){
+
+    function cerrar_banner() {
         $("#pop_up").remove();
     }
 
-    var imagenes=document.querySelectorAll("#tablaImg .img");
-    for(var i=0;i<imagenes.length;i++){
-        imagenes[i].addEventListener("click" ,comprar_articulo, false);
+
+    //Tienda
+    var imagenes = document.querySelectorAll("#tablaImg .img");
+    for (var i = 0; i < imagenes.length; i++) {
+        imagenes[i].addEventListener("click", comprar_articulo, false);
     }
 
-    function comprar_articulo(param){
-        if(param.target.id=="but1"){
+    function comprar_articulo(param) {
+        if (param.target.id == "but1") {
             $("#but1").replaceWith("<img src='images/sold.gif'>");
-        }else if(param.target.id=="but2"){
+        } else if (param.target.id == "but2") {
             $("#but2").replaceWith("<img src='images/sold.gif'>");
-        }else if(param.target.id=="but3"){
+        } else if (param.target.id == "but3") {
             $("#but3").replaceWith("<img src='images/sold.gif'>");
-        }else if(param.target.id=="but4"){
+        } else if (param.target.id == "but4") {
             $("#but4").replaceWith("<img src='images/sold.gif'>");
         }
     }
-    butCom.addEventListener("click" ,resaltarL1, false);
-    function resaltarL1(){
+
+    //Botones
+    butCom.addEventListener("click", resaltarL1, false);
+    function resaltarL1() {
         $(".link1").addClass("resaltar");
         $(".link2").removeClass("resaltar");
+        $(".link6").removeClass("resaltar");
     }
-    butTools.addEventListener("click" ,resaltarL2, false);
-    function resaltarL2(){
+    butTools.addEventListener("click", resaltarL2, false);
+    function resaltarL2() {
         $(".link2").addClass("resaltar");
         $(".link1").removeClass("resaltar");
+        $(".link6").removeClass("resaltar");
     }
-    butTog.addEventListener("click" ,togAll, false);
-    function togAll(){
+    butBrow.addEventListener("click", resaltarL6, false);
+    function resaltarL6() {
+        $(".link6").addClass("resaltar");
+        $(".link2").removeClass("resaltar");
+        $(".link1").removeClass("resaltar");
+    }
+    butTog.addEventListener("click", togAll, false);
+    function togAll() {
         $(".link2").toggleClass("resaltar");
         $(".link1").toggleClass("resaltar");
+        $(".link6").toggleClass("resaltar");
     }
-    butClear.addEventListener("click" ,clearAll, false);
-    function clearAll(){
+    butClear.addEventListener("click", clearAll, false);
+    function clearAll() {
         $(".link2").removeClass("resaltar");
         $(".link1").removeClass("resaltar");
+        $(".link6").removeClass("resaltar");
     }
-    butCorr.addEventListener("click" ,corrAll, false);
-    function corrAll(){
+    butCorr.addEventListener("click", corrAll, false);
+    function corrAll() {
         $(".link2").addClass("resaltar");
         $(".link1").addClass("resaltar");
+        $(".link6").addClass("resaltar");
+    }
+    radCom.addEventListener("click", resaltarL3, false);
+    function resaltarL3() {
+        $(".link3").addClass("resaltar");
+        $(".link4").removeClass("resaltar");
+        $(".link5").removeClass("resaltar");
+    }
+    radTool.addEventListener("click", resaltarL4, false);
+    function resaltarL4() {
+        $(".link4").addClass("resaltar");
+        $(".link3").removeClass("resaltar");
+        $(".link5").removeClass("resaltar");
+    }
+    radBrow.addEventListener("click", resaltarL5, false);
+    function resaltarL5() {
+        $(".link5").addClass("resaltar");
+        $(".link3").removeClass("resaltar");
+        $(".link4").removeClass("resaltar");
+    }
+
+    //Target
+    var imagenes = document.querySelectorAll("#tablaImg .img");
+    for (var i = 0; i < imagenes.length; i++) {
+        imagenes[i].addEventListener("click", comprar_articulo, false);
+    }
+
+    function comprar_articulo(param) {
+        if (param.target.id == "but1") {
+            $("#but1").replaceWith("<img src='images/sold.gif'>");
+        } else if (param.target.id == "but2") {
+            $("#but2").replaceWith("<img src='images/sold.gif'>");
+        } else if (param.target.id == "but3") {
+            $("#but3").replaceWith("<img src='images/sold.gif'>");
+        } else if (param.target.id == "but4") {
+            $("#but4").replaceWith("<img src='images/sold.gif'>");
+        }
     }
 
 
-    /*
-    but1.addEventListener("click", comprar_articulo1, false);
-    but2.addEventListener("click", comprar_articulo2, false);
-    but3.addEventListener("click", comprar_articulo3, false);
-    but4.addEventListener("click", comprar_articulo4, false);
 
-    function comprar_articulo1(){
-        $("#but1").replaceWith("<img src='images/sold.gif'>");
-    }
-    function comprar_articulo2(){
-        $("#but2").replaceWith("<img src='images/sold.gif'>");
-    }
-    function comprar_articulo3(){
-        $("#but3").replaceWith("<img src='images/sold.gif'>");
-    }
-    function comprar_articulo4(){
-        $("#but4").replaceWith("<img src='images/sold.gif'>");
-    }
-    */
-    
+
+
 
     /*
     .html inserta texto cambiandolo
